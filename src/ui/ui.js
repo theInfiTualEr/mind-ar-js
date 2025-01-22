f49c25810fb35825b03d0341a292e3a69f46d068
theInfiTualEr <47613413+theInfiTualEr@users.noreply.github.com>
2025-01-22 12:54:42 +0330

    Update ui.js to append ui element to the specificed container, not to body
    This wouldn't work unless you also change three.js to pass the container to the constructor of UI

--- src/ui/ui.js
+++ src/ui/ui.js
@@ -6,24 +6,24 @@ import scanningHTML from './scanning.html?raw';
 const css=`.mindar-ui-overlay{display:flex;align-items:center;justify-content:center;position:absolute;left:0;right:0;top:0;bottom:0;background:transparent;z-index:2}.mindar-ui-overlay.hidden{display:none}.mindar-ui-loading .loader{border:16px solid #222;border-top:16px solid white;opacity:.8;border-radius:50%;width:120px;height:120px;animation:spin 2s linear infinite}@keyframes spin{0%{transform:rotate(0)}to{transform:rotate(360deg)}}.mindar-ui-compatibility .content{background:black;color:#fff;opacity:.8;text-align:center;margin:20px;padding:20px;min-height:50vh}@media (min-aspect-ratio: 1/1){.mindar-ui-scanning .scanning{width:50vh;height:50vh}}@media (max-aspect-ratio: 1/1){.mindar-ui-scanning .scanning{width:80vw;height:80vw}}.mindar-ui-scanning .scanning .inner{position:relative;width:100%;height:100%;opacity:.8;background:linear-gradient(to right,white 10px,transparent 10px) 0 0,linear-gradient(to right,white 10px,transparent 10px) 0 100%,linear-gradient(to left,white 10px,transparent 10px) 100% 0,linear-gradient(to left,white 10px,transparent 10px) 100% 100%,linear-gradient(to bottom,white 10px,transparent 10px) 0 0,linear-gradient(to bottom,white 10px,transparent 10px) 100% 0,linear-gradient(to top,white 10px,transparent 10px) 0 100%,linear-gradient(to top,white 10px,transparent 10px) 100% 100%;background-repeat:no-repeat;background-size:40px 40px}.mindar-ui-scanning .scanning .inner .scanline{position:absolute;width:100%;height:10px;background:white;animation:move 2s linear infinite}@keyframes move{0%,to{top:0%}50%{top:calc(100% - 10px)}}`;
 
 export class UI {
-  constructor({uiLoading, uiScanning, uiError}) {
+  constructor({uiLoading, uiScanning, uiError, container}) {
     const cssBlock=document.createElement('style');
     cssBlock.innerText=css;
     document.head.appendChild(cssBlock);
     if (uiLoading === 'yes') {
-      this.loadingModal = this._loadHTML(loadingHTML);
+      this.loadingModal = this._loadHTML(loadingHTML, container);
     } else if (uiLoading !== 'no') {
       this.loadingModal = document.querySelector(uiLoading);
     }
 
     if (uiError === 'yes') {
-      this.compatibilityModal = this._loadHTML(compatibilityHTML);
+      this.compatibilityModal = this._loadHTML(compatibilityHTML, container);
     } else if (uiError !== 'no') {
       this.compatibilityModal = document.querySelector(uiError);
     }
 
     if (uiScanning === 'yes') {
-      this.scanningMask = this._loadHTML(scanningHTML);
+      this.scanningMask = this._loadHTML(scanningHTML, container);
     } else if (uiScanning !== 'no') {
       this.scanningMask = document.querySelector(uiScanning);
     }
@@ -58,11 +58,11 @@ export class UI {
     this.scanningMask.classList.add("hidden");
   }
 
-  _loadHTML(html) {
+  _loadHTML(html, container) {
     const e = document.createElement('template');
     e.innerHTML = html.trim();
     const rootNode = e.content.firstChild;
-    document.getElementsByTagName('body')[0].appendChild(rootNode);
+    container.appendChild(rootNode);
     return rootNode;
   }
 }
